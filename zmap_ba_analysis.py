63a395b1ceef4b8b70b10a0250e384cd6fb90818
JeeWin <jeewin@ACA801C0.ipt.aol.com>
2016-10-13 21:47:32 +0800

    重构

--- zmap_ba_analysis.py
+++ zmap_ba_analysis.py
@@ -6,21 +6,24 @@ import xlrd
 from tld import get_tld
 import socket
 
-if __name__ == "__main__":
+ 
+def get_ba_dic():    
     ba_data = xlrd.open_workbook('sjzx.xls')
     ba_table = ba_data.sheets()[0]
-    yba_dic = {} # 已备案
+    ba_dic = {} # 已备案ba_dic
     # nrows = table.nrows
 
     for i in range(6,55):# line range
-        if ba_table.row_values(i)[25] == '' and ba_table.row_values(i)[28] == '':
+        ips = ba_table.row_values(i)[25]
+        ports = ba_table.row_values(i)[26]
+        url = ba_table.row_values(i)[28]
+        if ips == '' and url == '':
             continue
         else:
-            ips = ba_table.row_values(i)[25]
-            if ips == '': # get url ip
-                if ba_table.row_values(i)[28][0:4]=='http':
+            if ips == '': 
+                if url[0:4]=='http':
                     try:
-                        ips = socket.getaddrinfo(get_tld(ba_table.row_values(i)[28]), 'http')[0][4][0]
+                        ips = socket.getaddrinfo(get_tld(url), 'http')[0][4][0] # get url ip
                     except Exception as e:
                         continue
                 else:
@@ -29,23 +32,25 @@ if __name__ == "__main__":
                 if not -1==ip.find('-'): # x.x.x.x-x
                     for j in range(int(ip[ip.rfind('.')+1:].split('-')[0]),int(ip[ip.rfind('.')+1:].split('-')[1])+1): # ip range
                         p = [] # port list
-                        for port in ba_table.row_values(i)[26].split(','):  # append port
+                        for port in ports.split(','):  # append port
                             if not -1 == port.find('-'): # x-x
                                 for k in range(int(port[port.rfind('p') + 1:].split('-')[0]),int(port[port.rfind('p') + 1:].split('-')[1]) + 1): # port range
                                     p.append((port[:3] + str(k)).encode('utf-8')) # tcpk
                             else:
                                 p.append(port.encode('utf-8'))
-                        yba_dic[(ip[:ip.rfind('.')+1]+str(j)).encode('utf-8')] = p # add ip
+                        ba_dic[(ip[:ip.rfind('.')+1]+str(j)).encode('utf-8')] = p # add ip
                 else: # x.x.x.x
                     p=[] # port list
-                    for port in ba_table.row_values(i)[26].split(','): # append port
+                    for port in ports.split(','): # append port
                         if not -1 == port.find('-'): # x-x
                             for j in range(int(port[port.rfind('p') + 1:].split('-')[0]),int(port[port.rfind('p') + 1:].split('-')[1]) + 1):
                                 p.append((port[:3]+str(j)).encode('utf-8'))
                         else:
                             p.append(port.encode('utf-8'))
-                    yba_dic[ip.encode('utf-8')] = p # add ip
-
+                    ba_dic[ip.encode('utf-8')] = p # add ip
+    return ba_dic
+    
+def get_zmap_dic():    
     zmap_results_list = []
     for line in open('sjzx_output.txt'):
         line = line.strip('\n')
@@ -58,14 +63,20 @@ if __name__ == "__main__":
         for j in zmap_results_list:
             if j[0]==i[0]:
                 zmap_results_dic[i[0]].append('tcp'+j[1])
+    return zmap_results_dic
 
-    for i in zmap_results_dic: # find wba
-        if yba_dic.has_key(i): # 已备案
-            for j in zmap_results_dic[i]:
-                if j not in yba_dic[i]:
+if __name__ == "__main__": 
+    
+    zmap_results = get_zmap_dic()
+    ba_results = get_ba_dic()    
+    
+    for i in zmap_results: # find wba
+        if ba_results.has_key(i): # 已备案
+            for j in zmap_results[i]:
+                if j not in ba_results[i]:
                     print i + ':' + j + '  not found'
         else:
-            for j in zmap_results_dic[i]:
+            for j in zmap_results[i]:
                 print i + ':' + j + '  not found'
 
 
