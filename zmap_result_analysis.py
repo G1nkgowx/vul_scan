a74d3a210174f5122c52ab21d9ee5359aef4e820
JeeWin <jeewin@JeeWinMBP.local>
2016-10-18 07:03:17 +0800

    增加功能

--- zmap_result_analysis.py
+++ zmap_result_analysis.py
@@ -8,11 +8,12 @@ from IPy import IP
 def get_zmap_dic():
     ip_range = IP('10.223.0.0/18')
     zmap_results_list = []
-    for line in open('sjzx_output.txt'):
+    for line in open('sjzx_all.txt'):
         line = line.strip('\n')
         if not line.split(',')[0] == "saddr":
             ip = line.split(',')[0]
             if ip in ip_range:
+                #print line.split(',')[0] + ',' + line.split(',')[1]
                 zmap_results_list.append([line.split(',')[0], line.split(',')[1]])
 
     zmap_results_dic = {}
@@ -25,5 +26,13 @@ def get_zmap_dic():
 
 if __name__ == "__main__":
     dic = get_zmap_dic()
-    print dic['10.223.50.98']
+    # print dic['10.223.50.1']
+    # print ""
+    # print dic['10.223.2.60']
+    # print ""
+    # print dic['10.223.2.61']
+    # print ""
+    # print dic['10.223.2.43']
+
+
 
